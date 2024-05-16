2b885f10f93ae8bb22c9911ae490ac940d3a7c31
Agasthya Gangavarapu <50297836+yoaugust@users.noreply.github.com>
2024-05-15 23:33:35 -0700

    core: Figured out initial libraries and began basic structure.

---
+++ main.py
@@ -0,0 +1,11 @@
+# Core Math Libraries
+import numpy as np
+from scipy.special import expit as sigmoid # For Sigmoid Activation
+from scipy.optimize import minimize # For building optimizer
+import matplotlib.pyplot as plt
+import pandas as pd
+import math
+import random
+
+
+# Imma try to do this in one file and then abstract it into smaller files if it gets bigger, lol.
